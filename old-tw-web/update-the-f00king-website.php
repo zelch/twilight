51e0cf5c6c285bef43d0b42e3a29ca528036422f
knghtbrd <knghtbrd@d7cf8633-e32d-0410-b094-e92efae38249>
2001-07-22 15:52:06 +0000

    rewrote update script so it cannot be accidentally used
    git-svn-id: svn://svn.icculus.org/twilight/trunk@276 d7cf8633-e32d-0410-b094-e92efae38249

--- old-tw-web/update-the-f00king-website.php
+++ old-tw-web/update-the-f00king-website.php
@@ -1,6 +1,12 @@
 <?php
-	header('Content-type: text/plain');
-	echo('okay.');
-	touch('/home/groups/t/tw/twilight/pulse/update-website');
+	header ("Content-type: text/plain");
+
+	if (ereg ("curl", $HTTP_USER_AGENT) || $force == "yes")
+	{
+		echo ("Requesting webpulse.\n");
+		touch ("/home/groups/t/tw/twilight/pulse/update-website");
+	} else {
+		echo ("I'm sorry Dave, but I'm afraid I can't do that.");
+	}
 ?>
 
