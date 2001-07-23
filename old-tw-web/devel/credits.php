b9ce862d8ab670a3028b3e00db5958579bc6fd18
knghtbrd <knghtbrd@d7cf8633-e32d-0410-b094-e92efae38249>
2001-07-23 04:37:26 +0000

    Cleaned up the footer, this is probably how it'll have to be done.
    git-svn-id: svn://svn.icculus.org/twilight/trunk@280 d7cf8633-e32d-0410-b094-e92efae38249

---
+++ old-tw-web/devel/credits.php
@@ -0,0 +1,12 @@
+<?php
+	$page = "Development";
+	$pageroot = "..";
+	require ($pageroot . "/include/header.php");
+
+	// yeah this is a nono - but it works and it's a stub anyway..
+	echo ("<font size=\"+2\">Project Credits</font>");
+
+	echo ("<p>This page has not been written yet.</p>");
+
+	require ($pageroot . "/include/footer.php");
+?>
--- old-tw-web/include/footer.php
+++ old-tw-web/include/footer.php
@@ -1,7 +1,10 @@
 <?php
 	box ("footer",
-		"FIXME: Something about Copyright here, preferably not to " .
-		"any one person<br>" .
+		"Contributions to Project Twilight are Copyright &copy; by their " .
+		"submitters.<br>" .
+		"See <a href=\"/devel/credits.php\">credits</a> for details</a><br>" .
+		"Quake&reg; and QuakeWorld&reg; are Copyright &copy; 1996-1997 by " .
+		"<a href=\"http://www.idsoftware.com/\">Id Software, Inc</a><br>" .
 		"Quake&reg; and QuakeWorld&reg; are registered trademarks " .
 		"of <a href=\"http://www.idsoftware.com/\">Id Software, Inc</a>"
 	);
