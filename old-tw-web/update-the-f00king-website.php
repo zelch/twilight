7b5fbfe971982d61efd3d1f9287c45f35145361f
knghtbrd <knghtbrd@d7cf8633-e32d-0410-b094-e92efae38249>
2001-09-05 16:55:43 +0000

    Tell you how long you'll be waiting for the webpulse..
    git-svn-id: svn://svn.icculus.org/twilight/trunk@640 d7cf8633-e32d-0410-b094-e92efae38249

--- old-tw-web/update-the-f00king-website.php
+++ old-tw-web/update-the-f00king-website.php
@@ -3,8 +3,9 @@
 
 	if (ereg ("curl", $HTTP_USER_AGENT) || $force == "yes")
 	{
-		echo ("Requesting webpulse.\n");
+		$sec = 160 - ($time() % 160);
 		touch ("/home/groups/t/tw/twilight/pulse/update-website");
+		echo ("Webpulse should happen in $sec\n");
 	} else {
 		echo ("I'm sorry Dave, but I'm afraid I can't do that.");
 	}
