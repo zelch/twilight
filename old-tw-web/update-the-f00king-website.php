4eff1ba7e1396a4c30c8b1fe092f0abf8a57c81f
knghtbrd <knghtbrd@d7cf8633-e32d-0410-b094-e92efae38249>
2001-09-05 19:09:07 +0000

    One more change, cosmetic this time.
    git-svn-id: svn://svn.icculus.org/twilight/trunk@644 d7cf8633-e32d-0410-b094-e92efae38249

--- old-tw-web/update-the-f00king-website.php
+++ old-tw-web/update-the-f00king-website.php
@@ -5,7 +5,7 @@
 	{
 		$sec = 160 - time() % 160;
 		touch ("/home/groups/t/tw/twilight/pulse/update-website");
-		echo ("Webpulse should happen in $sec\n");
+		echo ("Webpulse should happen in $sec seconds\n");
 	} else {
 		echo ("I'm sorry Dave, but I'm afraid I can't do that.");
 	}
