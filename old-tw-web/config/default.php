61f9804edaaecde0e312c13c7b121b2c2c5fce0f
knghtbrd <knghtbrd@d7cf8633-e32d-0410-b094-e92efae38249>
2001-09-09 07:55:30 +0000

    Change the menu order a little to emphasize the contact info a bit.
    git-svn-id: svn://svn.icculus.org/twilight/trunk@704 d7cf8633-e32d-0410-b094-e92efae38249

--- old-tw-web/config/default.php
+++ old-tw-web/config/default.php
@@ -218,17 +218,17 @@ a {
 			"Other Files" => "/files/other.php" ,
 			"CVS Snapshots" => "/cvs/index.php"
 		) ,
+		"Contacting Us" => array (
+			"Mailing Lists" => "/lists.php" ,
+			"IRC Channel" => "/irc.php" ,
+			"Developers" => "/people.php"
+		) ,
 		Development => array (
 			"Bug Tracking" => "/devel/index.php" ,
 			"Credits" => "/devel/credits.php"
 		) ,
 		Links => array (
 			"Links" => "/links.php"
-		) ,
-		Contact => array (
-			"Mailing Lists" => "/lists.php" ,
-			"IRC Channel" => "/irc.php" ,
-			"Developers" => "/people.php"
 		)
 	);
 
