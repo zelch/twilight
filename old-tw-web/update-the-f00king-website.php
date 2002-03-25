b3de4a260b55d59e14f002d5013574a3936eb809
knghtbrd <knghtbrd@d7cf8633-e32d-0410-b094-e92efae38249>
2002-03-25 06:22:22 +0000

    Shows how often the website has gotten updated recently...  Fixed the counter values.
    git-svn-id: svn://svn.icculus.org/twilight/trunk@1672 d7cf8633-e32d-0410-b094-e92efae38249

--- old-tw-web/update-the-f00king-website.php
+++ old-tw-web/update-the-f00king-website.php
@@ -3,7 +3,7 @@
 
 	if (ereg ("curl", $HTTP_USER_AGENT) || $force == "yes")
 	{
-		$sec = 120 - time() % 120;
+		$sec = 120 - time() % (15 * 60);
 		touch ("/home/groups/t/tw/twilight/pulse/update-website");
 		echo ("Webpulse should happen in $sec seconds\n");
 	} else {
