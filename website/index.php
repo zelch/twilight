4711e9da67c0c554ba03eda75475cea52b01cce2
knghtbrd <knghtbrd@d7cf8633-e32d-0410-b094-e92efae38249>
2002-07-07 23:36:54 +0000

    News update - still no dynamic news, but that's a little less critical for the time being.  Also, removed some of the old placeholder news items.. They didn't look very professional, really.  Note, this will automatically update the beta website directory, I hope.
    git-svn-id: svn://svn.icculus.org/twilight/trunk@2006 d7cf8633-e32d-0410-b094-e92efae38249

--- website/index.php
+++ website/index.php
@@ -5,24 +5,20 @@ require "include/main.php";
 
 startpage ("News");
 
+newsitem ("Sunday, 7 July 2002",
+		"The website is now in CVS properly, with webpulse script.  " .
+		"Knghtbrd apologizes for the delay in getting the new site up and " .
+		"running, and thanks everyone for their patience.  He's got a todo " .
+		"list that would scare most anyone and this website comprises but " .
+		"half a dozen items on it.  Coming soon, download pages and CVS " .
+		"snapshots."
+);
+
 newsitem ("Tuesday, 25 June 2002",
 		"The website (such that it is) has been converted to PHP.  " .
 		"It's still not classifyable as a real website, but at least it's " .
 		"now a mockup written in PHP rather than XHTML.  there is ".
 		"still not one whit of non-CSS support yet, sorry."
-);
-
-newsitem ("Thursday, 20 June 2002",
-		"The template for the new website looks non-sucky finally!  " .
-		"That's right, after many many long hours of fucking around with " .
-		"Mozilla, GIMP, vim, and an armload of reference manuals and " .
-		"websites, Knghtbrd is finally satisfied.  This looks nifty " .
-		"enough."
-);
-
-newsitem ("Wednesday, 19 June 2002",
-		"Something nifty happened on or about this time.  Really it " .
-		"did!"
 );
 
 endpage ();
