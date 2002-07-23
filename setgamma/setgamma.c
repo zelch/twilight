7bf45bb8fba54a47c536565371f949b1549540bc
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2002-07-23 12:26:16 +0000

    removed unneeded Get function for 3DFX
    git-svn-id: svn://svn.icculus.org/twilight/trunk@2069 d7cf8633-e32d-0410-b094-e92efae38249

--- setgamma/setgamma.c
+++ setgamma/setgamma.c
@@ -21,6 +21,17 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdio.h>
 #include <math.h>
 
+//johnfitz -- add support for standalone 3dfx cards (voodoo 1/2/rush)
+#include <string.h>
+#include <GL/gl.h>
+
+typedef int (WINAPI * RAMPFUNC)();
+//RAMPFUNC wglGetDeviceGammaRamp3DFX;
+RAMPFUNC wglSetDeviceGammaRamp3DFX;
+const char *gl_extensions;
+int vid_3dfxgamma;
+//johnfitz
+
 int setgamma(double gamma)
 {
 	int i;
@@ -29,6 +40,18 @@ int setgamma(double gamma)
 	WORD gammaramps[3][256];
 
 	hdc = GetDC (NULL);
+
+	//johnfitz -- add support for standalone 3dfx cards (voodoo 1/2/rush)
+	gl_extensions = glGetString (GL_EXTENSIONS);
+	if (strstr(gl_extensions, "WGL_3DFX_gamma_control"))
+	{
+		printf("detected WGL_3DFX_gamma_control\n"); //TEST
+		wglGetDeviceGammaRamp3DFX = (RAMPFUNC) wglGetProcAddress("wglGetDeviceGammaRamp3DFX");
+		vid_3dfxgamma = 1;
+	}
+	else
+		vid_3dfxgamma = 0;
+	//johnfitz
 
 	// LordHavoc: dodge the math
 	if (gamma == 1)
@@ -50,7 +73,14 @@ int setgamma(double gamma)
 		}
 	}
 
-	i = SetDeviceGammaRamp(hdc, &gammaramps[0][0]);
+	//johnfitz -- add support for standalone 3dfx cards (voodoo 1/2/rush)
+	if (vid_3dfxgamma)
+		i = wglSetDeviceGammaRamp3DFX(hdc, &gammaramps[0][0]);
+	// LordHavoc: changed to call both if 3dfx is available
+	//else
+		i = SetDeviceGammaRamp(hdc, &gammaramps[0][0]);
+	//johnfitz
+
 	ReleaseDC (NULL, hdc);
 	if (i)
 	{
@@ -64,12 +94,19 @@ int setgamma(double gamma)
 	}
 }
 
+void usage(void)
+{
+	printf("setgamma 1.1 by Forest \"LordHavoc\" Hale and John \"metlslime\" Fitzgibbons\n");
+	printf("usage: setgamma gamma\n");
+	printf("example gamma values: 1.0 (normal), 2.0 (double brightness), 1.35 (35% above normal)\n");
+}
+
 int main(int argc, char **argv)
 {
 	double gamma;
 	if (argc == 1)
 	{
-		printf("usage: setgamma gamma\nexample gamma values: 1.0 (normal), 2.0 (double brightness), 1.35 (35% above normal)\n");
+		usage();
 		printf("restoring gamma to default (1.0)\n");
 		return setgamma(1.0);
 	}
@@ -79,6 +116,6 @@ int main(int argc, char **argv)
 		if (gamma > 0)
 			return setgamma(gamma);
 	}
-	printf("usage: setgamma gamma\nexample gamma values: 1.0 (normal), 2.0 (double brightness), 1.35 (35% above normal)\n");
+	usage();
 	return 1;
 }
