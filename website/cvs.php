c0ae362690256e6087e07d04cf3a267c09267398
warp <warp@d7cf8633-e32d-0410-b094-e92efae38249>
2003-01-27 10:53:52 +0000

    Fix a typo, give an indication of where to actually go to get stuff.
    git-svn-id: svn://svn.icculus.org/twilight/trunk@2707 d7cf8633-e32d-0410-b094-e92efae38249

--- website/cvs.php
+++ website/cvs.php
@@ -6,7 +6,7 @@ require "include/main.php";
 startpage ("CVS");
 
 echo ("There are multiple ways to access Twilight CVS:<ul>\n");
-echo ("<li>The icculus.org CVS <a href=\"http://cvs.icculus.org/horde/chora/cvs.php?rt=twilight\">web interface<\a>.\n");
+echo ("<li>The icculus.org CVS <a href=\"http://cvs.icculus.org/horde/chora/cvs.php?rt=twilight\">web interface</a>.\n");
 echo ("<li>Anonymous CVS: cvs -z3 -d:pserver:anonymous@cvs.icculus.org:/cvs/cvsroot/twilight/ co twilight\n");
 echo ("<li>Private CVS: cvs -z3 -d<user>@cvs.icculus.org:/cvs/cvsroot/twilight/ co twilight\n");
 echo ("</ul>\n");
--- website/download.php
+++ website/download.php
@@ -5,7 +5,8 @@ require "include/main.php";
 
 startpage ("Download");
 
-echo ("This page isn't done yet...\n");
+echo ("This page isn't done yet...<br>\n");
+echo ("So go <a href=\"../release/\">here</a> for now.\n");
 
 endpage ();
 
