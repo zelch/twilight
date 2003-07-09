6714009735dd92f343040c5d9a935c1788eb0d3b
knghtbrd <knghtbrd@d7cf8633-e32d-0410-b094-e92efae38249>
2003-07-09 10:19:27 +0000

    A little polish
    git-svn-id: svn://svn.icculus.org/twilight/trunk@3214 d7cf8633-e32d-0410-b094-e92efae38249

--- website/download.php
+++ website/download.php
@@ -6,7 +6,7 @@ require "include/main.php";
 startpage ("Download");
 
 
-echo ("<p>Here is the current release of Project: Twilight.</p>\n");
+echo ("<p>Here is the current release of Project: Twilight:</p>\n");
 
 echo ("<div class=\"files\">\n");
 foreach (glob("$webspace/release/*.zip") as $filename)
@@ -17,8 +17,8 @@ foreach (glob("$webspace/release/*.zip") as $filename)
 }
 echo ("</div>");
 
-echo ("<p>Older releases can be found <a href=\"$urlbase/release/archive\">"
-		. "here</a></p>\n");
+echo ("<p>Some older releases can be found "
+		. "<a href=\"$urlbase/release/archive\">here</a></p>\n");
 
 echo ("<p>Other files which might come in handy:</p>\n");
 echo ("<div class=\"files\">\n");
