0a9d87fad2b3206f447db4bf2d9452139bdaac6f
knghtbrd <knghtbrd@d7cf8633-e32d-0410-b094-e92efae38249>
2003-07-09 10:07:26 +0000

    This may even work.
    git-svn-id: svn://svn.icculus.org/twilight/trunk@3210 d7cf8633-e32d-0410-b094-e92efae38249

--- website/download.php
+++ website/download.php
@@ -8,11 +8,14 @@ startpage ("Download");
 
 echo ("Here is the current release of Project: Twilight\n");
 
+echo ("<div class=\"files\">\n");
 foreach (glob("$webspace/release/*.zip") as $filename)
 {
 	$filename = basename($filename);
-	echo ("<br>$filename\n");
+	$url = "$urlbase/release/$filename";
+	echo ("<br><a href=\"$url\">$filename</a>\n");
 }
+echo ("</div>");
 
 echo ("This page isn't done yet...<br>\n");
 echo ("So go <a href=\"/twilight/release/\">here</a> for now.\n");
