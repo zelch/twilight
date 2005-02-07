5933938c2fee9d5eb9a5a7ed4b247a25cbe4c6c3
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2005-02-07 16:26:34 +0000

    corrected web cvs link and anonymous cvs explanation
    git-svn-id: svn://svn.icculus.org/twilight/trunk@5009 d7cf8633-e32d-0410-b094-e92efae38249

--- website/cvs.php
+++ website/cvs.php
@@ -6,8 +6,8 @@ require "include/main.php";
 startpage ("CVS");
 
 echo ("There are multiple ways to access Twilight CVS:<ul>\n");
-echo ("<li>The icculus.org CVS <a href=\"http://cvs.icculus.org/horde/chora/cvs.php?rt=twilight\">web interface</a>.\n");
-echo ("<li>Anonymous CVS: cvs -z3 -d:pserver:anonymous@cvs.icculus.org:/cvs/cvsroot/twilight/ co twilight\n");
+echo ("<li>The icculus.org CVS <a href=\"http://cvs.icculus.org/cvs/twilight\">web interface</a>.\n");
+echo ("<li>Anonymous CVS: <code>cvs -z3 -d:pserver:anonymous@cvs.icculus.org:/cvs/cvsroot/twilight/ login</code> (password: anonymous) followed by <code>cvs -z3 -d:pserver:anonymous@cvs.icculus.org:/cvs/cvsroot/twilight/ co twilight</code>\n");
 echo ("<li>Private CVS: cvs -z3 -d<user>@cvs.icculus.org:/cvs/cvsroot/twilight/ co twilight\n");
 echo ("</ul>\n");
 
